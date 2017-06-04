0d28c01ae14965464211fa989e589d3916011daf
Yubo Zhou <yubo@cse.yorku.ca>
2017-06-04 01:12:38 -0700

    add

--- prototype/lstm.py
+++ prototype/lstm.py
@@ -9,14 +9,14 @@ num_classes = 2
 train_samples = 1000
 test_samples = 100
 x_train = [[[1,1], [1,2]] for _ in range(train_samples)]
-y_train = [[[1,0],[0,1]] for _ in range(train_samples)]
-x_train = pad_sequences(x_train, maxlen=timesteps, padding='post')
+y_train = [[0,1] for _ in range(train_samples)]
+x_train = pad_sequences(x_train, maxlen=timesteps, padding='pre')
 y_train = pad_sequences(y_train)
 # x_train = np.array(x_train).reshape((2, timesteps, data_dim))
 
 # exit()
 x_val = [[[1,1], [1,2]] for _ in range(test_samples)]
-y_val = [[[1,0],[0,1]] for _ in range(test_samples)]
+y_val = [[0,1] for _ in range(test_samples)]
 
 # sample_weights = [[-1, 1]]
 
@@ -25,9 +25,9 @@ model = Sequential()
 model.add(LSTM(32, return_sequences=True,
                input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
 model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-# model.add(LSTM(32, return_sequences=True))  # return a single vector of dimension 32
-model.add(LSTM(num_classes, return_sequences=True, activation='softmax'))  # return a single vector of dimension 32
-# model.add(Dense(num_classes, activation='softmax'))
+model.add(LSTM(32, return_sequences=False))  # return a single vector of dimension 32
+# model.add(LSTM(num_classes, return_sequences=True, activation='softmax'))  # return a single vector of dimension 32
+model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
@@ -39,7 +39,7 @@ model.fit(x_train, y_train,
           batch_size=64, epochs=5,
           validation_data=(x_val, y_val))
 
-question = np.array([[[1,2]]])
+question = np.array([[[1,1], [1,2]]])
 question = pad_sequences(question, maxlen=timesteps, padding='pre')
 print(question)
 ret = model.predict(question, )
