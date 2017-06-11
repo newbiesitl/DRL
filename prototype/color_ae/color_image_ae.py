00041b6f30cef7246a5ed7431d8f8e1fd50e87d2
Yubo Zhou <yubo@cse.yorku.ca>
2017-06-11 12:41:14 -0700

    color pipeline is wokring now, next step is to visulize the results

--- prototype/color_ae/color_image_ae.py
+++ prototype/color_ae/color_image_ae.py
@@ -1,18 +1,26 @@
 from utils.utils import *
 import os
 from embedding.greedy_encoding import GreedyEncoder
+from prototype.color_ae.model_config.model_config import *
 
 cur_dir = os.path.dirname(__file__)
 data_folder = os.path.join(cur_dir, 'data')
 
 model_name = 'c_4000_2000_1000'
 model_folder = os.path.join(cur_dir, 'models')
-
+model_config = c_4000_2000_1000
 ae = GreedyEncoder()
-ae.load(os.path.join(model_folder, model_name), model_name)
 t =ImageTransformer()
-t.configure((80,60))
+t.configure(output_shape)
 dat = t.transform_all(data_folder, grey_scale=False, batch_size=200)
-for batch in dat:
-    print(batch.shape)
+dat = list(dat)[0]
+
+ae.set_training_data(dat, dat)
+ae.set_test_data(dat, dat)
+ae.compile(use_bias=True)
+ae.arch(model_config)
+ae.set_batch_size(20)
+ae.fit()
+# for batch in dat:
+#     print(batch.shape)
 
---
+++ prototype/color_ae/model_config/model_config.py
@@ -0,0 +1,114 @@
+import keras
+output_shape = (60, 40)
+# output_shape = (28, 28)
+color_img = True
+if color_img:
+    input_dim = output_shape[1] * output_shape[0] * 3
+else:
+    input_dim = output_shape[1] * output_shape[0]
+use_bias = True
+greedy_epoch = 20
+fine_tuning_epoch = 40
+activation_1 = 'linear'
+activation_2 = 'sigmoid'
+stack_activation = 'sigmoid'
+optimizer = keras.optimizers.Adadelta()
+# optimizer = keras.optimizers.Adagrad()
+
+
+c_2000_1000_300 = {
+    'stack': [
+        {
+            'input_dimension': input_dim,
+            'output_dimension': input_dim,
+            'embedding_dimension': 2048,
+            'activation_1': activation_1,
+            # 'activity_regularizer_1': regularizers.l2(10e-4),
+            'bias_1': use_bias,
+            'activation_2': activation_2,
+            # 'activity_regularizer_2': regularizers.l2(10e-4),
+            'bias_2': use_bias,
+            'optimizer': optimizer,
+            'loss_function': 'binary_crossentropy',
+            'epoch': greedy_epoch
+        },
+        {
+            'input_dimension': 2048,
+            'output_dimension': input_dim,
+            'embedding_dimension': 1024,
+            'activation_1': activation_1,
+            # 'activity_regularizer_1': regularizers.l2(10e-4),
+            'bias_1': use_bias,
+            'activation_2': activation_2,
+            # 'activity_regularizer_2': regularizers.l2(10e-4),
+            'bias_2': use_bias,
+            'optimizer': optimizer,
+            'loss_function': 'binary_crossentropy',
+            'epoch': greedy_epoch
+        },
+        {
+            'input_dimension': 1024,
+            'output_dimension': input_dim,
+            'embedding_dimension': 300,
+            'activation_1': activation_1,
+            # 'activity_regularizer_1': regularizers.l2(10e-4),
+            'bias_1': use_bias,
+            'activation_2': stack_activation,
+            # 'activity_regularizer_2': regularizers.l2(10e-4),
+            'bias_2': use_bias,
+            'optimizer': optimizer,
+            'loss_function': 'binary_crossentropy',
+            'epoch': greedy_epoch
+        },
+    ],
+    'epoch': fine_tuning_epoch,
+    'optimizer': optimizer,
+    'loss_function': 'binary_crossentropy'
+}
+
+c_2000_1000_500 = c_2000_1000_300
+c_2000_1000_500['stack'][-1]['embedding_dimension'] = 500
+
+
+c_4000_2000_1000 = c_2000_1000_300
+c_4000_2000_1000['stack'][0]['embedding_dimension'] = 4000
+c_4000_2000_1000['stack'][1]['input_dimension'] = 4000
+c_4000_2000_1000['stack'][1]['embedding_dimension'] = 2000
+c_4000_2000_1000['stack'][-1]['input_dimension'] = 2000
+c_4000_2000_1000['stack'][-1]['embedding_dimension'] = 1000
+
+
+c_4000_2000_1000_2000_4000 = c_4000_2000_1000
+c_4000_2000_1000_2000_4000['stack'].append(
+{
+            'input_dimension': 1000,
+            'output_dimension': input_dim,
+            'embedding_dimension': 2000,
+            'activation_1': activation_1,
+            # 'activity_regularizer_1': regularizers.l2(10e-4),
+            'bias_1': use_bias,
+            'activation_2': stack_activation,
+            # 'activity_regularizer_2': regularizers.l2(10e-4),
+            'bias_2': use_bias,
+            'optimizer': optimizer,
+            'loss_function': 'binary_crossentropy',
+            'epoch': greedy_epoch
+        }
+)
+
+c_4000_2000_1000_2000_4000['stack'].append(
+{
+            'input_dimension': 2000,
+            'output_dimension': input_dim,
+            'embedding_dimension': 4000,
+            'activation_1': activation_1,
+            # 'activity_regularizer_1': regularizers.l2(10e-4),
+            'bias_1': use_bias,
+            'activation_2': stack_activation,
+            # 'activity_regularizer_2': regularizers.l2(10e-4),
+            'bias_2': use_bias,
+            'optimizer': optimizer,
+            'loss_function': 'binary_crossentropy',
+            'epoch': greedy_epoch
+        }
+)
