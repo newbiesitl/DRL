bb5e661241e8414a5dbe5287537aeeec9535fb00
Yubo Zhou <yubo@cse.yorku.ca>
2017-06-11 19:51:53 -0700

    color encoder reproduce poort results

--- prototype/color_ae/color_image_ae.py
+++ prototype/color_ae/color_image_ae.py
@@ -6,21 +6,28 @@ from prototype.color_ae.model_config.model_config import *
 cur_dir = os.path.dirname(__file__)
 data_folder = os.path.join(cur_dir, 'data')
 
-model_name = 'c_4000_2000_1000'
+model_name = 'c_2000_1000_300'
 model_folder = os.path.join(cur_dir, 'models')
-model_config = c_4000_2000_1000
-ae = GreedyEncoder()
-t =ImageTransformer()
-t.configure(output_shape)
-dat = t.transform_all(data_folder, grey_scale=False, batch_size=200)
-dat = list(dat)[0]
+model_config = c_2000_1000_300
+train = True
 
-ae.set_training_data(dat, dat)
-ae.set_test_data(dat, dat)
-ae.compile(use_bias=True)
-ae.arch(model_config)
-ae.set_batch_size(20)
-ae.fit()
+ae = GreedyEncoder(verbose=True)
+if train:
+    t =ImageTransformer()
+    t.configure(output_shape)
+    dat = t.transform_all(data_folder, grey_scale=False, batch_size=200)
+    dat = list(dat)[0]
+
+    ae.set_training_data(dat, dat)
+    ae.set_test_data(dat, dat)
+    ae.compile(use_bias=True)
+    ae.arch(model_config)
+    ae.set_batch_size(20)
+    ae.fit()
+    ae.save(model_folder, model_name)
+else:
+    ae.load(model_folder, model_name)
+visualize_result_ae(ae, dat, output_shape, color_img=True)
 # Next step is to visualize the result from prediction
 # for batch in dat:
 #     print(batch.shape)
