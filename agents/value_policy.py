26f6104b9213696bcb6416f0422c010c9cea083f
charleschou <yubo@cse.yorku.ca>
2017-08-17 08:59:59 -0700

    adfs

--- agents/value_policy.py
+++ agents/value_policy.py
@@ -5,15 +5,20 @@ class Prototype(object):
     def __init__(self):
         pass
 
-    def get_value(self, state, action):
+    def value_sampler(self, state, action):
         '''
-        Value function uses value network to predict final rewards. The value network can have multiple forms of inputs:
+        Value network to predict final rewards. The value network can have multiple forms of inputs:
         1. State - use state to predict rewards
         2. Action - use action to predict rewards
         3. State, Action - use both State and Action to predict rewards, for example the Q function Q(s,a)
         :param state: current state
         :param action: the action with associate predictive rewards
-        :return: rewards
+        :return: rewards (1-D array)
         '''
 
-    def get_action(self, ):
+    def policy_sampler(self, state):
+        '''
+        Policy network to sample actions based on state, use as approximation to reduce search complexity
+        :param state:
+        :return:
+        '''
